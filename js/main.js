6d28afa0f67550e566b46ad184a5f698ddde0423
Kevinlevin200 <codigo27ocho42020@gmail.com>
2025-10-30 11:37:24 -0500

    feat: :sparkles: Subiendo codigos
    By @Kevin Santiago Rivero Rueda

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginBtn = document.getElementById("loginBtn");
+  const registerBtn = document.getElementById("registerBtn");
+  const volverBtn = document.getElementById("volverBtn");
+  const inicio = document.getElementById("inicio");
+  const formulario = document.getElementById("formulario");
+  const formTitle = document.getElementById("formTitle");
+
+  loginBtn.addEventListener("click", () => {
+    formTitle.textContent = "Iniciar sesión";
+    formulario.classList.remove("hidden");
+    inicio.classList.add("hidden");
+  });
+
+  registerBtn.addEventListener("click", () => {
+    formTitle.textContent = "Registrarse";
+    formulario.classList.remove("hidden");
+    inicio.classList.add("hidden");
+  });
+
+  volverBtn.addEventListener("click", () => {
+    formulario.classList.add("hidden");
+    inicio.classList.remove("hidden");
+  });
+
+  document.getElementById("authForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Formulario enviado (sin conexión al backend aún)");
+  });
+});
