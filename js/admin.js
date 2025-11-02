f16b2794fe3aff82910da2ecff56054f9e6d43f9
Juancamilorojasarenas <juancarojasar@gmail.com>
2025-11-02 18:55:37 -0500

    feat: :sparkles: nueva coneccion
    nuevaconeccion
    
    by@ juan camilo rojas arenas

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:4000/api/v1";
+const API_BASE = "https://ranking-system-backend.onrender.com";
 const token = localStorage.getItem("token");
 let adminId = null;
 let editandoCategoria = false;
--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:4000/api/v1";
+const API_BASE = "https://ranking-system-backend.onrender.com";
 
 console.log("🚀 login.js cargado");
 
